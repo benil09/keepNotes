519807f66a06c946832369d51f00c63a1833a855
benil09 <benil8235@gmail.com>
2025-06-30 08:38:49 +0530

    ready for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoute from "./Routes/notes.routes.js"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
@@ -10,17 +11,31 @@ dotenv.config()
 
 const app=express();
 const PORT = process.env.PORT || 8000 ;
+const __dirname=path.resolve();
 
-
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin:"http://localhost:5173",
     
 }))
+
+}
+
+
 app.use(express.json()) // allows us to get the values such as req.body or req.params 
 app.use(rateLimiter)
 
 
 app.use("/api/notes",notesRoute)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+
+}
+
 
 
 connectDB().then(()=>{
