d242269cb805fc85b489a345117f48b26e835220
benil09 <benil8235@gmail.com>
2025-08-17 10:11:40 +0530

    improved reddis error

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,18 +1,19 @@
-import ratelimit from "../config/upstash.js"
+import ratelimit from "../config/upstash.js";
 
-const rateLimiter = async (req,res,next)=>{
-        try {
-            const {success} = await ratelimit.limit("my-limit-key");
-            if(!success) return res.status(429).json({
-                message:"Too many requests ! Try after some time"
-            })
-            next()
-        } catch (error) {
-            console.log("Rate limit error " , error)
-            next(error);
-            
-        }
+const rateLimiter = async (req, res, next) => {
+  try {
+    const ip = req.ip || req.headers["x-forwarded-for"] || "global"; // fallback if IP not found
 
-}
+    const { success } = await ratelimit.limit(`limit_${ip}`);
+    if (!success)
+      return res.status(429).json({
+        message: "Too many requests ! Try after some time",
+      });
+    next();
+  } catch (error) {
+    console.log("Rate limit error ", error);
+    next(error);
+  }
+};
 
-export default rateLimiter
+export default rateLimiter;
